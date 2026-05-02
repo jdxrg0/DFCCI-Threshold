7c4cd4b9c7ecc6c76a1cbd5e13d14b4234c72a78
d0ul0s <traps-wavy-gristle@duck.com>
2026-05-02 21:36:29 +0800

    Switch to Port 587 and STARTTLS to bypass Render firewall

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -14,8 +14,9 @@ const sendEmail = async (to, subject, html, retries = 3, backoff = 1000) => {
 
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false, // Upgrades to TLS using STARTTLS
+      requireTLS: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD,
